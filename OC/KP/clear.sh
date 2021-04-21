0dc80c3e83e640305f8c05a7055f6362e6fe3e42
VSNelyubin <71440609+VSNelyubin@users.noreply.github.com>
2021-04-21 18:42:37 +0300

    Update clear.sh

--- OC/KP/clear.sh
+++ OC/KP/clear.sh
@@ -2,9 +2,9 @@
 for pid in $(ps -ef | awk '/c.out/ {print $2}'); do kill -9 $pid; done
 for pid in $(ps -ef | awk '/b.out/ {print $2}'); do kill -9 $pid; done
 for pid in $(ps -ef | awk '/a.out/ {print $2}'); do kill -9 $pid; done
-unexpand node2.cpp > tmpfile1
-cat tmpfile1 > node2.cpp
-unexpand interface.cpp > tmpfile2
-cat tmpfile2 > interface.cpp
-unexpand node3.cpp > tmpfile3
-cat tmpfile3 > node3.cpp
+unexpand c.cpp > tmpfile1
+cat tmpfile1 > c.cpp
+unexpand a.cpp > tmpfile2
+cat tmpfile2 > a.cpp
+unexpand b.cpp > tmpfile3
+cat tmpfile3 > b.cpp
