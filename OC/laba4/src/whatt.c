8001e2ba56906416ec59dae36b05d3b60b119976
VSNelyubin <71440609+VSNelyubin@users.noreply.github.com>
2021-04-21 19:33:06 +0300

    Update and rename OC/laba4/whatt.c to OC/laba4/src/whatt.c

--- OC/laba4/src/whatt.c
+++ OC/laba4/src/whatt.c
@@ -7,8 +7,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
-
-
 int main(){
 	int num=255;
 	char*inpt2=malloc(sizeof(char)*num);
@@ -20,51 +18,27 @@ int main(){
 			inpt=malloc(sizeof(char)*num);
 			for(int ee=0;ee<num;ee++){
 				inpt[ee]=inpt2[ee];
-//				printf("%d\n",((inpt2[ee]=='\0')||(inpt2[ee]=='\n')));
 			}
 			free(inpt2);
 			break;
 		}
 	}
-
 	int fd = open(inpt, O_RDWR);
         if (fd == -1){printf("error opening file\n");}
 	char ch;
 
         size_t sz;
 	sz = lseek(fd, 0, SEEK_END);
-
 	void*mapped=mmap(NULL, sz, PROT_READ|PROT_WRITE,MAP_SHARED, fd,0);
 	close(fd);
-
 	if(mapped==NULL){printf("bad mmap\n");return -1;}
-//	else{printf("good mmap @ %llu\n",(unsigned long long)mapped);}
-
 	char*flhr=(char*)mapped;
-
 	char*backup=malloc(sizeof(char)*sz);
 	if(backup==NULL){return -1;}
 	for(int i=0;i<sz;i++){
 		backup[i]=flhr[i];
 	}
-
-/*	for(int i=0;i<sz;i++){
-		printf("%c ",flhr[i]);
-	}printf("\n");*/
-
 	pid_t pid;
-
-/*	unsigned long long adres=(unsigned long long)mapped;
-	for(int i=0;adres>>i;i++){printf("%d",(adres>>i)&1);
-		if(i%5==4){printf("\t");}
-	}
-	printf("\n");
-	for(int i=0;adres>>i;i+=5){printf("%d\t",(adres>>i)&31);}
-	printf("\n");
-
-	printf("%llu %llu\n", mapped,adres);
-*/
-
 	int ci,cim=0;
 
 	char*szarg;
@@ -78,29 +52,8 @@ int main(){
 		szarg[cim]='0'+(sz/ci)%10;
                 cim++;
         }
-
-/*	for(ci=0;adres>>(5*ci);ci++);
-	inpt=realloc(inpt,sizeof(char)*ci);
-	cim=ci;
-	ci--;
-	for(ci;ci+1;ci--){
-		fd=(adres>>(5*ci))&31;
-		if(fd<10){fd+='0';}
-		else{fd+='a'-10;}
-		inpt[cim-ci-1]=fd;
-	}
-	inpt[cim]='\0';
-
-	printf("not split yet %llu\n",flhr);
-        for(int i=0;i<sz;i++){
-        	printf("%c ",flhr[i]);
-        }printf("\n");*/
-
-//	printf("inpt is %s\n",inpt);
-
 	pid=fork();
 	if(pid==0){
-//		printf("child, but not B yet %llu\n",flhr);
 		char*args[]={"./b.out",inpt,szarg,NULL};
 		execvp(args[0],args);
 		_exit (EXIT_FAILURE);
