4334ca3979c84c91c33d44b4cc84866559d87c17
VSNelyubin <71440609+VSNelyubin@users.noreply.github.com>
2021-04-21 19:43:03 +0300

    Delete process.h

--- OC/process.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PROCESS_H__
-#define __PROCESS_H__
-
-#include <signal.h>
-
-extern void sign_handler(int sig, struct sigcontext *sc);
-extern void alarm_handler(int sig, struct sigcontext *sc);
-#endif
